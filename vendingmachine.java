73c6f90c4b1baee07b73d6cb9051cb2e0ae66052
Henokk-14 <>
2021-02-11 12:59:38 -0500

    jello

--- vendingmachine.java
+++ vendingmachine.java
@@ -1,1 +1,14 @@
 // Testing
+public class vendingmachine {
+  // counts down form 10
+    public static void main(String[] args) {
+      int test = 10;
+      int count = 0;
+      while(test >= 1){
+        System.out.println(test);
+        test--;
+        count++;
+      }
+      System.out.println("Amount of numbers printed is: " + count);
+    }
+}
